324c76946973e82b4cfd159efe7136df192e29c0
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-11-26 01:29:56 +0100

    Add form validation to email

---
+++ js/form-validation.js
@@ -0,0 +1,13 @@
+const errorMessage = document.querySelector('.error-message');
+const form = document.getElementById('contact-form');
+const email = document.getElementById('email');
+const emailRegex = /^[a-z0-9_.]+@[a-z0-9_.]+\.[a-z0-9_.]+$/;
+
+form.addEventListener('submit', (e) => {
+  if (!emailRegex.test(email.value)) {
+    e.preventDefault();
+    errorMessage.style.display = 'flex';
+    email.style.border = "1px solid var(--error-color)";
+    email.style.borderRadius = "1rem";
+  }
+});
