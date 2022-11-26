66cc7d7640118579d72ec68b99bc4e6c4b18a00a
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-11-26 17:51:59 +0100

    Store user data in local browser

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,13 +1,33 @@
+// FORM VALIDATION
 const errorMessage = document.querySelector('.error-message');
 const form = document.getElementById('contact-form');
+const userName = document.getElementById('name');
+const message = document.getElementById('message');
 const email = document.getElementById('email');
 const emailRegex = /^[a-z0-9_.]+@[a-z0-9_.]+\.[a-z0-9_.]+$/;
 
 form.addEventListener('submit', (e) => {
   if (!emailRegex.test(email.value)) {
-    e.preventDefault();
-    errorMessage.style.display = 'flex';
+    errorMessage.style.display = 'block';
     email.style.border = '1px solid var(--error-color)';
     email.style.borderRadius = '1rem';
   }
+});
+
+// LOCAL STORAGE
+let localData = {
+  name: '',
+  email: '',
+  message: '',
+};
+
+function storeLocalData() {
+  localStorage.setItem('user', JSON.stringify(localData));
+}
+
+form.addEventListener('change', () => {
+  localData.name = userName.value;
+  localData.email = email.value;
+  localData.message = message.value;
+  storeLocalData();
 });
