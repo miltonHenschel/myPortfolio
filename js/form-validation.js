4f7d5181eee3b4d630da30e65003eb27d2f6563c
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-11-26 04:35:34 +0100

    Fix linters

--- js/form-validation.js
+++ js/form-validation.js
@@ -6,5 +6,6 @@ const emailRegex = /^[a-z0-9_.]+@[a-z0-9_.]+\.[a-z0-9_.]+$/;
 form.addEventListener('submit', (e) => {
   if (!emailRegex.test(email.value)) {
     e.preventDefault();
-   }
+    errorMessage.style.display = 'flex';
+  }
 });
